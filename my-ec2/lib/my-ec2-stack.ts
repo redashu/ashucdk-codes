91f5dd20cf5695dcbab86a61d1f9b0244ef0c8b6
redashu <ashutoshh@linux.com>
2025-01-08 03:32:37 +0530

    ec2 creawte code

--- my-ec2/lib/my-ec2-stack.ts
+++ my-ec2/lib/my-ec2-stack.ts
@@ -1,16 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from  'aws-cdk-lib/aws-ec2';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class MyEc2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // define default vpc to use from existing aws region 
+    const vpc = ec2.Vpc.fromLookup(this,'ExistingVPC' ,{
+      isDefault: true
+  });
+  // creating ec2 instace 
+  const ashuinstance = new ec2.Instance(this,'ashumachine',{
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyEc2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+      vpc,  
+      instanceType: new ec2.InstanceType('t2.micro'),
+      machineImage: new ec2.AmazonLinuxImage(),
+      keyName: 'splunk-key',
+  });
   }
 }
